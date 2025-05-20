4fea6d782f7210703b4b77f4e439c3d1a35f3412
JANBEAST\Jan <janstigli@gmail.com>
2025-05-20 08:29:30 +0200

    gff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { authGuard } from './auth-guard/auth-guard.component';
 
 export const routes: Routes = [
-    { path: "list", component: ListComponent },
+    { path: "list", component: ListComponent, canActivate: [authGuard],},
     { path: "login", component: LoginComponent },
     { path: "register", component: RegistrationComponent },
-    { path: "**", redirectTo: "/list", pathMatch: "full" }
+    { path: "**", redirectTo: "/list", pathMatch: "full", canActivate: [authGuard], }
 ];
---
+++ src/app/auth-guard/auth-guard.component.ts
@@ -0,0 +1,16 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthServService } from '../services/Auth/auth-serv.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authServ = inject(AuthServService);
+  console.log("we're in, onee-sama!")
+  const router = inject(Router)
+  if (authServ.isAuth()){
+    return true;
+  } else { 
+    console.log("You shall not pas")
+    // return createUrlTreeFromSnapshot(route, ['/login']);
+    return router.createUrlTree(['/login'])
+  }
+};
